bf99e61565434b9c0a2adcd3bff7f4da55bcc44c
Sai1Ram <sairamsenapati0022@gmail.com>
2024-08-17 19:12:02 +0530

    change in timeout while connection

--- src/helper/socket.js
+++ src/helper/socket.js
@@ -3,7 +3,7 @@ export const initSocket = async () => {
     const option = {
         'force new connection': true,
         reconnectionAttempts: 'Infinity',
-        timeout: 10000,
+        timeout: 20000,
         transports: ['websocket']
     }
     return io(process.env.REACT_APP_SERVER_URL, option);
